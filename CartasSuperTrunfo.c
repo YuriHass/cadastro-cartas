b9e6de840853358b57a98f182047020711ab92fe
YMH <53067859+YuriHass@users.noreply.github.com>
2025-09-22 03:38:56 +0000

    refactor: changed PIB to GDP for correct English nomenclature

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,12 +5,12 @@ float calculate_density(int population, float area) {
   return population / area;
 }
 
-float calculate_pib_per_capita(float pib, int population) {
-  return (pib * 1000000000.0f) / population;
+float calculate_gdp_per_capita(float gdp, int population) {
+  return (gdp * 1000000000.0f) / population;
 }
 
-float calculate_super_power(unsigned long int population, float area, float pib, int attractions, float pib_per_capita, float density) {
-    return population + area + pib + attractions + pib_per_capita + (1000000.0f / density);
+float calculate_super_power(unsigned long int population, float area, float gdp, int attractions, float gdp_per_capita, float density) {
+    return population + area + gdp + attractions + gdp_per_capita + (1000000.0f / density);
 }
 
 int main() {
@@ -19,7 +19,7 @@ int main() {
   char card_1_code[4], card_2_code[4], card_1_name[50], card_2_name[50];
   unsigned long int card_1_population, card_2_population;
   int card_1_tourist_attractions, card_2_tourist_attractions;
-  float card_1_area, card_2_area, card_1_pib, card_2_pib, card_1_density, card_2_density, card_1_pib_capita, card_2_pib_capita, card_1_super_power, card_2_super_power;
+  float card_1_area, card_2_area, card_1_gdp, card_2_gdp, card_1_density, card_2_density, card_1_gdp_capita, card_2_gdp_capita, card_1_super_power, card_2_super_power;
   short comparison_result, winner_card;
 
   // Área para entrada de dados da cidade 1
@@ -41,14 +41,14 @@ int main() {
   scanf("%f", &card_1_area);
 
   printf("Insira o PIB (bilhões de reais) da cidade: ");
-  scanf("%f", &card_1_pib);
+  scanf("%f", &card_1_gdp);
 
   printf("Insira a numero de pontos turisticos da cidade: ");
   scanf("%d", &card_1_tourist_attractions);
 
   card_1_density = calculate_density(card_1_population, card_1_area);
-  card_1_pib_capita = calculate_pib_per_capita(card_1_pib, card_1_population);
-  card_1_super_power = calculate_super_power(card_1_population, card_1_area, card_1_pib, card_1_tourist_attractions, card_1_pib_capita, card_1_density);
+  card_1_gdp_capita = calculate_gdp_per_capita(card_1_gdp, card_1_population);
+  card_1_super_power = calculate_super_power(card_1_population, card_1_area, card_1_gdp, card_1_tourist_attractions, card_1_gdp_capita, card_1_density);
 
   // Área para entrada de dados da cidade 2
   printf("\n*****Dados da segunda cidade*****\n");
@@ -69,14 +69,14 @@ int main() {
   scanf("%f", &card_2_area);
 
   printf("Insira o PIB (bilhões de reais) da cidade: ");
-  scanf("%f", &card_2_pib);
+  scanf("%f", &card_2_gdp);
 
   printf("Insira a numero de pontos turisticos da cidade: ");
   scanf("%d", &card_2_tourist_attractions);
 
   card_2_density = calculate_density(card_2_population, card_2_area);
-  card_2_pib_capita = calculate_pib_per_capita(card_2_pib, card_2_population);
-  card_2_super_power = calculate_super_power(card_2_population, card_2_area, card_2_pib, card_2_tourist_attractions, card_2_pib_capita, card_2_density);
+  card_2_gdp_capita = calculate_gdp_per_capita(card_2_gdp, card_2_population);
+  card_2_super_power = calculate_super_power(card_2_population, card_2_area, card_2_gdp, card_2_tourist_attractions, card_2_gdp_capita, card_2_density);
 
   // Área para exibição dos dados das cidades
   printf("\nCidades cadastradas com sucesso!\n\n");
@@ -86,10 +86,10 @@ int main() {
   printf("Nome da Cidade: %s\n", card_1_name);
   printf("População: %ld\n", card_1_population);
   printf("Área: %.2f km²\n", card_1_area);
-  printf("PIB: %.2f bilhões de reais\n", card_1_pib);
+  printf("PIB: %.2f bilhões de reais\n", card_1_gdp);
   printf("Número de pontos turísticos : %d\n", card_1_tourist_attractions);
   printf("Densidade populacional: %.2f hab/km²\n", card_1_density);
-  printf("PIB per Capita: %.2f reais\n", card_1_pib_capita);
+  printf("PIB per Capita: %.2f reais\n", card_1_gdp_capita);
   printf("Super poder: %.6f\n", card_1_super_power);
   
   printf("\nCarta 2: \n");
@@ -98,10 +98,10 @@ int main() {
   printf("Nome da Cidade: %s\n", card_2_name);
   printf("População: %ld\n", card_2_population);
   printf("Área: %.2f km²\n", card_2_area);
-  printf("PIB: %.2f bilhões de reais\n", card_2_pib);
+  printf("PIB: %.2f bilhões de reais\n", card_2_gdp);
   printf("Número de pontos turísticos : %d\n", card_2_tourist_attractions);
   printf("Densidade populacional: %.2f hab/km²\n", card_2_density);
-  printf("PIB per Capita: %.2f reais\n", card_2_pib_capita);
+  printf("PIB per Capita: %.2f reais\n", card_2_gdp_capita);
   printf("Super poder: %.6f\n", card_2_super_power);
 
   // Área para comparação dos dados entre as cidades
@@ -115,7 +115,7 @@ int main() {
   winner_card = 2 - comparison_result;
   printf("Área: Carta %hd venceu (%hd)\n", winner_card, comparison_result);
 
-  comparison_result = card_1_pib > card_2_pib;
+  comparison_result = card_1_gdp > card_2_gdp;
   winner_card = 2 - comparison_result;
   printf("PIB: Carta %hd venceu (%hd)\n", winner_card, comparison_result);
 
@@ -127,7 +127,7 @@ int main() {
   winner_card = 2 - comparison_result;
   printf("Densidade populacional: Carta %hd venceu (%hd)\n", winner_card, comparison_result);
 
-  comparison_result = card_1_pib_capita > card_2_pib_capita;
+  comparison_result = card_1_gdp_capita > card_2_gdp_capita;
   winner_card = 2 - comparison_result;
   printf("PIB per Capita: Carta %hd venceu (%hd)\n", winner_card, comparison_result);
 
